0ad60347f5c08f28c462318b0cb73d99385ab892
Atul15-2003 <atulkmaurya1403@gmail.com>
2025-09-13 12:47:16 +0530

    Fixed base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter} from 'react-router-dom'
 import App from './App.jsx'
 import Stairs from './components/common/stairs.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Stairs />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
 
